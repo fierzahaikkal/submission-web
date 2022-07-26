d0f30e287010601d5eb0ba5eb2e561fe490d9a4c
fierzahaikkal <ojogames.id@gmail.com>
2022-07-27 01:00:51 +0700

    addNotHandler finish commit

--- src/handler.js
+++ src/handler.js
@@ -35,7 +35,6 @@ const addNoteHandler = (request, h) => {
     notes.push(newNote);
 
     const isSuccess = notes.filter((note) => note.name === name || note.readPage === readPage).length > 0;
-    const isName = notes.filter((note) => note.name === name).length > 0;
 
     if (isSuccess) {
         const response = h.respone({
@@ -48,5 +47,28 @@ const addNoteHandler = (request, h) => {
         response.code(201);
         return response;
     }
-
+    if (isSuccess !== note.name) {
+        const response = h.respone({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+    if (isSuccess >= note.pageCount) {
+        const response = h.respone({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+    const response = h.respone({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
 }
+
+module.exports = {addNoteHandler};
