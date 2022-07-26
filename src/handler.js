614a70a2f38416987027751978a8dcb77db4ba7e
fierzahaikkal <ojogames.id@gmail.com>
2022-07-27 00:42:00 +0700

    handler note commit

--- src/handler.js
+++ src/handler.js
@@ -14,6 +14,39 @@ const addNoteHandler = (request, h) => {
     } = request.payload;
 
     const id = nanoid(16);
-    const createdAt = new Date().toISOString();
-    const updatedAt = createdAt;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newNote = {
+        id,
+        name, 
+        year, 
+        author, 
+        summary, 
+        publisher, 
+        pageCount, 
+        readPage,
+        finished,
+        reading, 
+        insertedAt, 
+        updatedAt
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter((note) => note.name === name || note.readPage === readPage).length > 0;
+    const isName = notes.filter((note) => note.name === name).length > 0;
+
+    if (isSuccess) {
+        const response = h.respone({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookId: `${note.id}`
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
 }
--- src/notes.js
+++ src/notes.js
@@ -1,12 +1,2 @@
 const notes = [
-    {
-        "name": string,
-        "year": number,
-        "author": string,
-        "summary": string,
-        "publisher": string,
-        "pageCount": number,
-        "readPage": number,
-        "reading": boolean
-    }
 ]
